fa67d9ee4c728fc94f0d228f3ad9b9517bfe88a0
Mhytas <mathysdemon@icloud.com>
2024-11-12 15:03:49 +0100

    Optimisation + suppression de commentaire inutile de la commande /ping

--- Events/interactionCreate/Ping/interactionCreate.js
+++ Events/interactionCreate/Ping/interactionCreate.js
@@ -1,4 +1,4 @@
-const Discord = require("discord.js")
+const { EmbedBuilder, ButtonStyle, ButtonBuilder, ActionRowBuilder } = require("discord.js")
 const i18n = require('i18n');
 
 module.exports = async (bot, interaction) => {
@@ -9,32 +9,25 @@ module.exports = async (bot, interaction) => {
                 if(langue === "fr") i18n.setLocale("fr")
                 if(langue === "en") i18n.setLocale("en")
 
-                let reloadPing = new Discord.ActionRowBuilder()
-                .addComponents(
-                    new Discord.ButtonBuilder()
-                        .setCustomId("ping")
-                        .setEmoji("🔄")
-                        .setLabel(i18n.__("ping_actualiser"))
-                        .setStyle(Discord.ButtonStyle.Success)
+                let reloadPing = new ActionRowBuilder()
+                .addComponents(new ButtonBuilder()
+                    .setCustomId("ping")
+                    .setEmoji("🔄")
+                    .setLabel(i18n.__("ping_actualiser"))
+                    .setStyle(ButtonStyle.Success)
                 )
-                /*const pingUser = interaction.createdTimestamp - Date.now();
-                    let emojiUser;
-                    if(pingUser < 200) { emojiUser = "🟢" } 
-                    else if (pingUser < 400 && pingUser > 200) { emojiUser = "🟠" }
-                    else if(pingUser > 400) {emojiUser = "🔴" };
                 
-                    // Ping de l'API de discord*/
-                    let APIPing = bot.ws.ping
-                    let APIemoji;
-                    if(APIPing < 200) { APIemoji = "🟢" }
-                    else if(APIPing < 400 && APIPing >= 200) { APIemoji = "🟠" }
-                    else if(APIPing >= 400) {APIemoji = "🔴" }
+                // Ping de l'API de discord*/
+                let APIPing = bot.ws.ping
+                let APIemoji;
+                if(APIPing < 200) { APIemoji = "🟢" }
+                else if(APIPing < 400 && APIPing >= 200) { APIemoji = "🟠" }
+                else if(APIPing >= 400) {APIemoji = "🔴" }
         
-                let PingEmbed = new Discord.EmbedBuilder()
+                let PingEmbed = new EmbedBuilder()
                     .setDescription(APIemoji + i18n.__("ping_message") + `**${APIPing}ms**`)
                     .setColor(bot.color)
-                    //\`${emojiUser}\` Pong ! | Votre ping : **${pingUser}ms**\n
-        
+
                 await interaction.deferUpdate()
                 await interaction.editReply({embeds: [PingEmbed], components: [reloadPing]})
             })
