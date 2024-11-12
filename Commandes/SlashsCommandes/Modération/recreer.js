9c549ae710b1721507367c004d0937cf3b6be7f5
Mhytas <mathysdemon@icloud.com>
2024-11-12 11:47:07 +0100

    Traduction complète de la commande /recreer + ajout d'un message lors du clone du salon

--- Commandes/SlashsCommandes/Modération/recreer.js
+++ Commandes/SlashsCommandes/Modération/recreer.js
@@ -1,4 +1,5 @@
-const { PermissionFlagsBits, ChannelType, ApplicationCommandOptionType } = require("discord.js")
+const { PermissionFlagsBits, ChannelType, ApplicationCommandOptionType, EmbedBuilder, ButtonStyle, ButtonBuilder, ActionRowBuilder } = require("discord.js")
+const i18n = require('i18n');
 
 module.exports = {
     
@@ -59,21 +60,101 @@ module.exports = {
     ],
 
     async run(bot, message, args, db) {
-        
-        let channel = args.getChannel("salon")
-        if(!channel) channel = message.channel
-        if(channel.id !== message.channel.id && !message.guild.channels.cache.get(channel.id)) return await message.reply({content: "Pas de salon trouvé !", ephemeral: true})
-        
-        /*
-        let category = args.getChannel("catégorie")
-        //Prévenir l'utilisateur qu'aucune catégorie n'a été renseigné, donc son salon se supprimé
-        */
-        
-        let channel_name = channel.name
-        
-        await channel.clone()
-        await channel.delete()
-        
-        await message.reply({content: `Le salon ${channel_name} a bien recréer !`, ephemeral: true})
-    }
+        await db.query(`SELECT * FROM server WHERE guild = '${message.guild.id}'`, async (err, req_langue) => {
+
+            let langue = req_langue[0].langue
+            if(langue === "fr") i18n.setLocale("fr")
+            if(langue === "en") i18n.setLocale("en")
+
+            let systeme = `/${i18n.__("recreer_system")}`
+            let temps_supression = 10
+
+            await message.deferReply({ ephemeral: true })
+            
+            let channel = args.getChannel("salon")
+            if(!channel) channel = message.channel
+            if(channel.id !== message.channel.id && !message.guild.channels.cache.get(channel.id)) {
+                
+                const embed_erreur_pas_de_salon = new EmbedBuilder()
+                .setColor("DarkRed")
+                .setAuthor({
+                    name: `${bot.user.username} - ${systeme}`,
+                    iconURL: `${bot.user.displayAvatarURL({dynamic: true})}`,
+                })
+                .setDescription(i18n.__("recreer_erreur_pas_de_salon"))
+                .setTimestamp()
+                .setFooter({text: systeme})
+
+                await message.editReply({embeds: [embed_erreur_pas_de_salon], ephemeral: true})
+                return
+            }
+
+            /*
+            let category = args.getChannel("catégorie")
+            //Prévenir l'utilisateur qu'aucune catégorie n'a été renseigné, donc son salon se supprimé
+            */
+
+            let new_channel = await channel.clone()
+            await channel.delete()
+
+            const embed_channel = new EmbedBuilder()
+            .setColor(bot.color)
+            .setAuthor({
+                name: `${bot.user.username} - ${systeme}`,
+                iconURL: `${bot.user.displayAvatarURL({dynamic: true})}`,
+            })
+            .setDescription(`${i18n.__("recreer_delete_message_le_salon")} **${channel.name}** ${i18n.__("recreer_delete_message_channel")}`)
+            .setTimestamp()
+            .setFooter({text: systeme})
+
+            const embed_message = new EmbedBuilder()
+            .setColor(bot.color)
+            .setAuthor({
+                name: `${bot.user.username} - ${systeme}`,
+                iconURL: `${bot.user.displayAvatarURL({dynamic: true})}`,
+            })
+            .setDescription(`${i18n.__("recreer_delete_message_le_salon")} **${channel.name}** ${i18n.__("recreer_delete_message_reply_P1")} <#${new_channel.id}>${i18n.__("recreer_delete_message_reply_P2")}`)
+            .setTimestamp()
+            .setFooter({text: systeme})
+
+            const bouton_message = new ActionRowBuilder().addComponents(new ButtonBuilder()
+                .setCustomId("Delete_message_recreer")
+                .setEmoji("🗑")
+                .setLabel(`${i18n.__("recreer_delete_message_bouton_P1")} ${temps_supression} ${i18n.__("recreer_delete_message_bouton_P2")} `)
+                .setStyle(ButtonStyle.Danger)
+                .setDisabled(true)
+            )
+            
+            await new_channel.send({content: `<@${message.user.id}>`, embeds: [embed_channel], components: [bouton_message]})
+            .then(msg => {
+
+                let message_secondes = i18n.__("recreer_delete_message_bouton_P2")
+
+                // Créer un intervalle pour mettre à jour le message chaque seconde
+                const countdown = setInterval(async () => {
+                    temps_supression--;
+                    if (temps_supression > 0) {
+                        if(temps_supression === 1 ) message_secondes = i18n.__("recreer_delete_message_bouton_P3")
+                        
+                            const bouton_message = new ActionRowBuilder().addComponents(new ButtonBuilder()
+                            .setCustomId("Delete_message_recreer")
+                            .setEmoji("🗑")
+                            .setLabel(`${i18n.__("recreer_delete_message_bouton_P1")} ${temps_supression} ${message_secondes}`)
+                            .setStyle(ButtonStyle.Danger)
+                            .setDisabled(true)
+                            )
+
+                        try { await msg.edit({components: [bouton_message]}) } catch {}
+                        
+                    } else {
+                        // Supprimer le message une fois le compte à rebours terminé
+                        try { await msg.delete().catch() } catch {}
+                        clearInterval(countdown); // Arrêter l'intervalle
+                    }
+                }, 1000); // Exécute toutes les 1000 millisecondes (1 seconde)
+            })
+            .catch();
+            try { await message.editReply({embeds: [embed_message]}) } catch {}
+        }
+    )}
 }
